5a793797f0cd46e8e8e2a72caf990652f9d25778
Rohan890 <league@WTS7.attlocal.net>
2018-09-03 18:51:12 -0700

    Nasty Surprise and part of SimonSays.

--- src/NastySurprise.java
+++ src/NastySurprise.java
@@ -1,34 +1,36 @@
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.net.MalformedURLException;
 import java.net.URL;
-
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
-public class NastySurprise {
+public class NastySurprise implements  MouseListener {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton trickbutton = new JButton();
+	JButton treatbuttons = new JButton();
 	public static void main(String[] args) {
 		NastySurprise nasty = new NastySurprise();
-		JFrame frame = new JFrame();
+	}	
+	public NastySurprise() {
 		frame.setVisible(true);
-		JPanel panel = new JPanel();	
-		frame.add(panel);
-		JButton trickbutton = new JButton();
+		frame.add(panel);		
+		trickbutton.addMouseListener(this);
 		panel.add(trickbutton);
-		JButton treatbuttons = new JButton();
 		panel.add(treatbuttons);
+		treatbuttons.addMouseListener(this);
 		JLabel label = new JLabel();
 		label.setText("Trick");
 		trickbutton.add(label);
 		JLabel labels = new JLabel();
 		labels.setText("Treat");
 		treatbuttons.add(labels);
-		
-		//have to add an action listener
-		
-	}
+		frame.pack();
+	}	
 	private void showPictureFromTheInternet(String imageUrl) {
 	     try {
 	          URL url = new URL(imageUrl);
@@ -41,5 +43,34 @@ public class NastySurprise {
 	     } catch (MalformedURLException e) {
 	          e.printStackTrace();
 	     }
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("clicked");
+		
+		if(e.getSource() == trickbutton) {
+			showPictureFromTheInternet("https://cdn.shopify.com/s/files/1/1368/5523/products/wink_poodle_F4_1024x1024.jpg?v=1527264086");
+		}
+		else if(e.getSource() == treatbuttons) {
+			showPictureFromTheInternet("https://i.pinimg.com/236x/18/77/97/1877975c291e712e9567692d80f664c3--creepy-dolls-creepy-art.jpg");
+		}
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub	
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub	
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
 	}
 	}
--- src/SimonSays.java
+++ src/SimonSays.java
@@ -22,7 +22,7 @@ public class SimonSays extends KeyAdapter {
 
 	// Complete steps 1 - 7 before you test
 	// 1. Declare a JFrame variable
-	
+	JFrame frame = new JFrame();
 	HashMap<Integer, String> images = new HashMap<Integer, String>();
 	private int imageIndex;
 	private int tries = 0;
@@ -32,11 +32,15 @@ public class SimonSays extends KeyAdapter {
 	private void makeAlbum() {
 		// 2. Add the four images that match keyboard keys like this: 
 		//images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
-
+		images.put(new Integer(KeyEvent.VK_UP), "up.jpg");
+		images.put(new Integer(KeyEvent.VK_DOWN), "down.png");
+		images.put(new Integer(KeyEvent.VK_LEFT), "left.png");
+		images.put(new Integer(KeyEvent.VK_RIGHT), "right.jpg");
 		// 3. Use a JOptionPane to tell the user the rules: "Press the matching key when
 		// 'Simon says' otherwise press a different key"
-		
+		JOptionPane.showMessageDialog(null, "Press the matching key when 'Simon says' otherwise press a different key.");
 		// 4. Call the showImage method to show an image
+		showImage();
 	}
 
 	public void keyPressed(KeyEvent e) {
@@ -69,29 +73,38 @@ public class SimonSays extends KeyAdapter {
 
 	private void showImage() {
 		// 5. Initialize your frame to a new JFrame()
-		
+		JFrame frame = new JFrame();
 		// 6. Set the frame to visible
-
+		frame.setVisible(true);
 		// 7. Uncomment the following line to add a random image to your frame
-		//frame.add(getNextRandomImage());
-
+		frame.add(getNextRandomImage());
+		
 		// 8. Set the name of your frame
-
+		frame.setName("frame");
 		// 9. Pack the frame
-		
+		frame.pack();
 		// 10. Set the defaultCloseOperation of your from to JFrame.EXIT_ON_CLOSE
-		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 11. Add a key listener to the frame
-
+		frame.addKeyListener(this);
 		// 12. Create a new instance of Random
-
+		Random r = new Random();
+		int n = r.nextInt(2);
 		// 13. Use the Random and the speak method to either say 
 		// "Simon says press this key" or "Press this key"
-
+		System.out.println(n);
+		if(n == 0) {
+			speak("Simon says press this key");
+		}
+		else if(n == 1) {
+			speak("Press this key");
+		}
 		// 14. Above, set the value of simonSays to true/false appropriately
 
 		
 	}
+
+	
 
 	private Component getNextRandomImage() {
 		this.imageIndex = new Random().nextInt(4) + 37;
